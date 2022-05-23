08214d04d3d22d6459265901ec662afe86c06fc4
kullop <burakcankarasel@gmail.com>
2022-05-23 11:24:18 +0300

    day05 project completed including script.js

--- day05/script.js
+++ day05/script.js
@@ -1,16 +1,16 @@
-const btnDOM = document.querySelector("#btn")
+const btnDOM = document.querySelector("#btn");
 
-const paraDOM = document.querySelector("#result")
+const paraDOM = document.querySelector("#result");
 
 function checkPalindrome(){
+    
+    const inputDOM = document.querySelector("#input");
 
-    const inputDOM = document.querySelector("#input")
+    let permVal = inputDOM.value.toLowerCase();
 
-    let permVal = inputDOM.value.toLowerCase()
+    let tempVal = permVal.split("").reverse().join("");
 
-    let tempVal = permVal.split("").reverse().join("")
-
-    tempVal === permVal ? paraDOM.innerHTML = `${inputDOM.value.toUpperCase()} is palindrome` : paraDOM.innerHTML = `${inputDOM.value.toUpperCase()} is not palindrome`
+    tempVal === permVal ? paraDOM.innerHTML = `${inputDOM.value.toUpperCase()} is palindrome` : paraDOM.innerHTML = `${inputDOM.value.toUpperCase()} is not palindrome`;
 }
 
 btnDOM.addEventListener("click", checkPalindrome)
